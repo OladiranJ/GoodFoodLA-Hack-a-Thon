172dbbdfb29bee93e22a1bd0a7acf9865932e3e2
Jeremy Yandell <jereyand@gmail.com>
2019-09-16 16:01:59 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 
 import './App.css';
 import database from '../src/components/Firebase/firebase'
+import Header from '../src/components/Header'
 
 
 class App extends Component {
-  componentDidMount(){
-    database.collection('users').doc().set({
-      email: 'asdfasdfsd', 
-      username: 'asdfasdfasd'
-  })
-  }
+  // componentDidMount(){
+  //   database.collection('users').doc().set({
+  //     email: 'asdfasdfsd', 
+  //     username: 'asdfasdfasd'
+  // })
+  // }
   render() {
     return (
+      
       <div className="App">
+        <Header />
         Hello World
       </div>
     );
