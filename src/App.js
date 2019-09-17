21eb7005ac281138085a2f5f6016ab4f8e732ed4
Jeremy Yandell <jereyand@gmail.com>
2019-09-16 22:09:28 -0700

    build data grid

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import './App.css';
 import database from '../src/components/Firebase/firebase'
 import Header from '../src/components/Header'
 import NavBar from './components/NavBar'
+import DataGrid from './components/DataGrid';
+import * as data from './constants/fair-json';
 
 
 class App extends Component {
-  // componentDidMount(){
-  //   database.collection('users').doc().set({
-  //     email: 'asdfasdfsd', 
-  //     username: 'asdfasdfasd'
-  // })
-  // }
+  componentDidMount(){
+    console.log(data.fairJSON);
+    // data.fairJSON.map(x => {
+    //   database.collection('fair').doc().set({
+    //     x
+    //   })
+    // })
+  }
   render() {
     return (
       
       <div className="App">
         <Header />
         <NavBar />
+        <DataGrid />
+        
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/constants/fair-json.js
@@ -0,0 +1,128 @@
+export const fairJSON = 
+[
+    {
+      "INDICATOR": "Total Food System Jobs in LA Food shed",
+      "BASELINE": "1,311,483; 13%",
+      "UPDATE": "1,464,450; 15%"
+    },
+    {
+      "INDICATOR": "Total Food System Jobs in LA County",
+      "BASELINE": "520,493; 13%",
+      "UPDATE": "586,061; 14%"
+    },
+    {
+      "INDICATOR": "Number of sidewalk food vendors in LA City",
+      "BASELINE": "12,000",
+      "UPDATE": "50,000"
+    },
+    {
+      "INDICATOR": "Number of traditional food trucks operating in LA County",
+      "BASELINE": "3,000",
+      "UPDATE": "2,858"
+    },
+    {
+      "INDICATOR": "Number of cities in LA County out of 88 cities that permit legal sidewalk food vending",
+      "BASELINE": 57,
+      "UPDATE": 57
+    },
+    {
+      "INDICATOR": "Average hourly wage of nonsupervisory food system worker in LA Foodshed",
+      "BASELINE": "$10.50",
+      "UPDATE": "$12.55"
+    },
+    {
+      "INDICATOR": "Average hourly wage of nonsupervisory food system worker in LA County",
+      "BASELINE": "$10.62",
+      "UPDATE": "$12.59"
+    },
+    {
+      "INDICATOR": "Gap between the living wage for a household with two adults & one child in LA County and minimum wage",
+      "BASELINE": "$12.07",
+      "UPDATE": "$16.16"
+    },
+    {
+      "INDICATOR": "Gap between the average hourly wage of all food system workers in LA Foodshed and average hourly wage of all workers",
+      "BASELINE": "$13.26",
+      "UPDATE": "$12.72"
+    },
+    {
+      "INDICATOR": "Gap between the average hourly wage of all food system workers in LA County and the average hourly wage of all workers",
+      "BASELINE": "$14.13",
+      "UPDATE": "$13.52"
+    },
+    {
+      "INDICATOR": "Average hourly wage of nonsupervisory food system worker in LA Foodshed",
+      "BASELINE": "$10.50",
+      "UPDATE": "$12.55"
+    },
+    {
+      "INDICATOR": "Average hourly wage of supervisory food system worker in LA Foodshed",
+      "BASELINE": "$18.46",
+      "UPDATE": "$19.80"
+    },
+    {
+      "INDICATOR": "Average hourly wage of nonsupervisory food system worker in LA County",
+      "BASELINE": "$10.62",
+      "UPDATE": "$12.59"
+    },
+    {
+      "INDICATOR": "Average hourly wage of supervisory food system worker in LA County",
+      "BASELINE": "$18.17",
+      "UPDATE": "$19.23"
+    },
+    {
+      "INDICATOR": "Average number of chickens used for meat per farm in California",
+      "BASELINE": "1,416,818",
+      "UPDATE": "1,787,706"
+    },
+    {
+      "INDICATOR": "Average number of hens used for eggs per farm in California",
+      "BASELINE": "532,498",
+      "UPDATE": "581,491"
+    },
+    {
+      "INDICATOR": "Average number of pigs per farm in California",
+      "BASELINE": "11,921",
+      "UPDATE": "10,965"
+    },
+    {
+      "INDICATOR": "Average number of cows used for dairy per farm in California",
+      "BASELINE": "1,521",
+      "UPDATE": "1,724"
+    },
+    {
+      "INDICATOR": "Reported injuries and illnesses due to agricultural production and pesticide exposure amongst agricultural workers in LA Foodshed",
+      "BASELINE": 60,
+      "UPDATE": 176
+    },
+    {
+      "INDICATOR": "California food system workers experiencing food insecurity",
+      "BASELINE": "18%",
+      "UPDATE": "26.50%"
+    },
+    {
+      "INDICATOR": "California workers experiencing food insecurity",
+      "BASELINE": "17%",
+      "UPDATE": "20.80%"
+    },
+    {
+      "INDICATOR": "California food system workers on CalFresh",
+      "BASELINE": "23%",
+      "UPDATE": "19.70%"
+    },
+    {
+      "INDICATOR": "California workers on CalFresh",
+      "BASELINE": "11%",
+      "UPDATE": "16.10%"
+    },
+    {
+      "INDICATOR": "Nonsupervisory food system workers in California with healthcare",
+      "BASELINE": "54%",
+      "UPDATE": "67.30%"
+    },
+    {
+      "INDICATOR": "Number of drivers with health benefits and increased wages covered under Gold Star contract for GFPP with LAUSD",
+      "BASELINE": 160,
+      "UPDATE": 450
+    }
+  ]
